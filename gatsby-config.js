3cc6e369655c69eb6f04c88fe0b186e0e978ac68
Kevin Cameron <kevinacameron@hotmail.com>
2020-05-15 17:42:00 -0700

    Update pkgs

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,12 @@ module.exports = {
       },
 
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-166828766-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
