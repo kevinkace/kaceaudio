3a071e9ef0544f21b665b272c0feda88cd09c3e1
Kevin Cameron <kevinacameron@hotmail.com>
2020-05-17 23:24:42 -0700

    Eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,13 @@ module.exports = {
     __PATH_PREFIX__: true,
   },
   extends: `react-app`,
+  rules : {
+    "key-spacing" : ["error", {
+      "align": {
+          "beforeColon": true,
+          "afterColon": true,
+          "on": "colon"
+      }
+  }]
+  }
 };
