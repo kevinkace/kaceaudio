50e849f88f743e9eb117a3ad81c5e7e567916393
Kevin Cameron <kevinacameron@hotmail.com>
2020-07-13 23:17:34 -0700

    format

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title       : `Kace`,
-    description : `music`,
-    author      : `@kaceaudio`,
+    title       : 'Kace',
+    description : 'music',
+    author      : '@kaceaudio',
   },
   plugins: [
-    `gatsby-transformer-csv`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-transformer-csv',
+    'gatsby-plugin-react-helmet',
     {
-      resolve : `gatsby-source-filesystem`,
+      resolve : 'gatsby-source-filesystem',
       options : {
-        name : `images`,
+        name : 'images',
         path : `${__dirname}/src/images`,
       },
     },
     {
-      resolve : `gatsby-source-filesystem`,
+      resolve : 'gatsby-source-filesystem',
       options : {
-        name : `data`,
+        name : 'data',
         path : `${__dirname}/src/data`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-postcss`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-postcss',
     {
-      resolve : `gatsby-plugin-manifest`,
+      resolve : 'gatsby-plugin-manifest',
       options : {
-        name             : `gatsby-starter-default`,
-        short_name       : `starter`,
-        start_url        : `/`,
-        background_color : `#111`,
-        theme_color      : `#0f1`,
-        display          : `minimal-ui`,
-        icon             : `src/images/icon.png`,
+        name             : 'gatsby-starter-default',
+        short_name       : 'starter',
+        start_url        : '/',
+        background_color : '#111',
+        theme_color      : '#0f1',
+        display          : 'minimal-ui',
+        icon             : 'src/images/icon.png',
       },
 
     },
     {
-      resolve : `gatsby-plugin-google-analytics`,
+      resolve : 'gatsby-plugin-google-analytics',
       options : {
-        trackingId: "UA-166828766-1",
+        trackingId: 'UA-166828766-1',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
