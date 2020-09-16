b75d71633314d1ced523d46eb5e051e1c60ba7a2
Kevin Cameron <kevinacameron@hotmail.com>
2020-09-16 11:33:45 -0700

    Add sets to arvhive, and move data

---
+++ src/data/archive.js
@@ -0,0 +1,47 @@
+export const songArchive = [{
+    year  : 2003,
+    songs : [{
+        title   : "Breakin'Borders Dub",
+        artist  : "Kace & Andyslim",
+        bitrate : 320,
+        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Breakin'Borders+Dub+-+Kace+%26+Andyslim.mp3"
+    }, {
+        title   : "Realize feat. MC Flight",
+        artist  : "Kace & Andyslim",
+        bitrate : 128,
+        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Realize+(feat+MC+Flight)+-+Kace+%26+Andyslim.mp3"
+    }, {
+        title   : "Freezeline",
+        artist  : "Kace & Andyslim",
+        bitrate : 320,
+        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Freezeline+-+Kace+%26+Andyslim.mp3"
+    }]
+}];
+
+export const setArchive = [{
+    year : 2003,
+    sets : [{
+        title     : "Oktober",
+        artist    : "Kace",
+        bitrate   : 320,
+        href      : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Oktober+Mix+-+Kace+(2003).mp3",
+        tracklist : [
+            "00:00 - Breakin'Borders Dub - Kace & Andyslim",
+            "02:09 - Astro-Sine - Chris SU - DSCI4005",
+            "06:29 - Antena Array - Clandestine",
+            "09:00 - Undercurrent - Stare & Phibbs - BLIND001",
+            "12:36 - Optic (Polar? VIP) - Default - REPA006",
+            "15:07 - Cell Block - Kace",
+            "18:21 - HideOut - Crossfire - 1210-006",
+            "22:41 - Drama - Axis of Evil - CELL001",
+            "26:38 - Ronin - Kiko & mc Mecha - 12KOHM",
+            "29:32 - Stand Down - Kace",
+            "33:29 - Flatworm - Optiv - RL001",
+            "37:49 - Carrier - Fierce & Cause4Concern - QRN002",
+            "39:36 - Egomatric (Rob F remix) - Typecell - PRTG003",
+            "43:34 - Secrets Inside - Paul B - DSCI4EP004",
+            "46:28 - Fatal Statistic - Raiden - TPT003",
+            "50:25 - Realize feat Flight - Kace & Andyslim",
+        ]
+    }]
+}];
--- src/pages/archive.js
+++ src/pages/archive.js
@@ -1,25 +1,12 @@
 
 import React from 'react';
 
-import Layout  from '../components/layout/layout';
-import SEO     from '../components/seo';
+import Layout from '../components/layout/layout';
+import SEO    from '../components/seo';
 
-const archive = [{
-    year  : 2003,
-    songs : [{
-        label   : "Breakin'Borders Dub",
-        bitrate : 320,
-        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/Breakin'Borders+Dub+-+Kace+%26+Andyslim.mp3"
-    }, {
-        label   : "Realize",
-        bitrate : 128,
-        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/Realize+(feat+MC+Flight)+-+Kace+%26+Andyslim.mp3"
-    }, {
-        label   : "Freezeline",
-        bitrate : 320,
-        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/Freezeline+-+Kace+%26+Andyslim.mp3"
-    }]
-}];
+import css from './archive.module.css';
+
+import { songArchive, setArchive } from '../data/archive';
 
 export default function ArchivePage() {
 
@@ -27,12 +14,27 @@ export default function ArchivePage() {
         <Layout>
             <SEO title='Kace Archive' description="You don't know where you're going if you don't know where you've been" />
 
-            {archive.map(({ year, songs }) =>
-                <div>
-                    <h2>{year}</h2>
+            <h2>Songs</h2>
+
+            {songArchive.map(({ year, songs }) =>
+                <div className={css.section}>
+                    <h3>{year}</h3>
                     <ul>
-                        {songs.map(({ href, label, bitrate }) =>
-                            <li>{label} - <a href={href}>mp3 ({bitrate})</a></li>
+                        {songs.map(({ href, title, artist, bitrate }) =>
+                            <li><strong>{title}</strong> - {artist} | <a href={href}>mp3 ({bitrate})</a></li>
+                        )}
+                    </ul>
+                </div>
+            )}
+
+            <h2>Sets</h2>
+
+            {setArchive.map(({ year, sets }) =>
+                <div className={css.section}>
+                    <h3>{year}</h3>
+                    <ul>
+                        {sets.map(({ href, title, artist, bitrate }) =>
+                            <li><strong>{title}</strong> - {artist} | <a href={href}>mp3 ({bitrate})</a></li>
                         )}
                     </ul>
                 </div>
