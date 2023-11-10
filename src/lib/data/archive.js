00c85586364b5f9774dd11a3c893e33ede7f6328
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-10 14:07:23 -0800

    archive

---
+++ src/lib/data/archive.js
@@ -0,0 +1,123 @@
+export const songArchive = [{
+    year  : 2003,
+    songs : [{
+        title   : 'Cell Block',
+        artist  : 'Kace',
+        bitrate : 320,
+        href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Cell+Block+-+Kace.mp3'
+    }, {
+        title   : "Breakin'Borders Dub",
+        artist  : 'Kace & Andyslim',
+        bitrate : 320,
+        href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Breakin'Borders+Dub+-+Kace+%26+Andyslim.mp3"
+    }, {
+        title   : 'Realize MC Flight',
+        artist  : 'Kace & Andyslim',
+        bitrate : 128,
+        href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Realize+(feat+MC+Flight)+-+Kace+%26+Andyslim.mp3'
+    }, {
+        title   : 'Freezeline',
+        artist  : 'Kace & Andyslim',
+        bitrate : 320,
+        href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Freezeline+-+Kace+%26+Andyslim.mp3'
+    }]
+}, {
+    year  : 2004,
+    songs : [{
+        title   : 'Sub Scrypt',
+        artist  : 'Kace & Andyslim',
+        bitrate : 320,
+        href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Sub+Scrypt+-+Kace+%26+Andyslim.mp3'
+    }]
+}, {
+    year  : 2006,
+    songs : [{
+        title   : 'Dust Bowl',
+        artist  : 'Kace',
+        bitrate : 320,
+        href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Dustbowl+-+Kace.mp3'
+    }]
+}];
+
+export const setArchive = [{
+    year : 2003,
+    sets : [{
+        title     : 'Oktober',
+        artist    : 'Kace',
+        bitrate   : 320,
+        href      : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Oktober+Mix+-+Kace+(2003).mp3',
+        tracklist : [
+            "Breakin'Borders Dub - Kace & Andyslim (00:00)",
+            'Astro-Sine - Chris SU - DSCI4005 (02:09)',
+            'Antena Array - Clandestine (06:29)',
+            'Undercurrent - Stare & Phibbs - BLIND001 (09:00)',
+            'Optic (Polar? VIP) - Default - REPA006 (12:36)',
+            'Cell Block - Kace (15:07)',
+            'HideOut - Crossfire - 1210-006 (18:21)',
+            'Drama - Axis of Evil - CELL001 (22:41)',
+            'Ronin - Kiko & mc Mecha - 12KOHM (26:38)',
+            'Stand Down - Kace (29:32)',
+            'Flatworm - Optiv - RL001 (33:29)',
+            'Carrier - Fierce & Cause4Concern - QRN002 (37:49)',
+            'Egomatric (Rob F remix) - Typecell - PRTG003 (39:36)',
+            'Secrets Inside - Paul B - DSCI4EP004 (43:34)',
+            'Fatal Statistic - Raiden - TPT003 (46:28)',
+            'Realize feat Flight - Kace & Andyslim (50:25)',
+        ]
+    }]
+}, {
+    year : 2004,
+    sets : [{
+        title     : 'Funk Fueled Fuck',
+        artist    : 'Kace & Andyslim',
+        bitrate   : 192,
+        href      : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Funk+Fueled+Fuck+-+Kace+%26+Andyslim+(2004).mp3',
+        tracklist : [
+            'Geiger - Optiv - Random (00:00)',
+            'Skyclad - Corrupt Souls - Armada (02:34)',
+            'Sabotage - Kiko & Mecha - ShadowLaw (06:58)',
+            'Asylum - Espionage - Unsigned (09:54)',
+            'The Chamber - Mayhem & Impulse - ShadowLaw (12:05)',
+            'Dreamstate - Clandestine - Unsigned (16:29)',
+            'Commodore - Dynafect - Unsigned (21:37)',
+            'Promist - Desimal - Unsigned (24:33)',
+            'The Sleeper - Static & rregula - Unsigned (29:23)',
+            "Don't you - Black Sun Empire - BSE (33:25)",
+            'Milkshake (Kiko remix) - Kelis - White (37:49)',
+            'Suppuku - Corrupt Souls - MovingShadow (41:07)',
+            'Retro (Noisia Remix) - Falcon - Citrus (45:09)',
+            'Tempered - Impulse - OhmResistance (48:49)',
+            'Orbiting Earth - Psidream - Habit (52:29)',
+            'Sub Scrypt - Kace & Andyslim - Unsigned (57:37)',
+            'Dragline - Desimal - Unsigned (61:28)',
+            'Haze - Kiko - ShadowLaw (65:34)'
+        ]
+    }, {
+        title     : 'Assimilation',
+        artist    : 'Kace & Andyslim',
+        bitrate   : 192,
+        href      : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Assimilation+-+Kace+%26+Andyslim+(2004).mp3',
+        tracklist : [
+            'Access Denied - Cyb Orc - Forthcoming Cabal',
+            'Leaving Marks - Glitch - Unsigned',
+            'Lockjaw - Noisia & Mayhem - Forthcoming Citrus',
+            'Move Right - Phace - Forthcoming Leet',
+            '2100 - Dynafect - unsigned',
+            'Booking - Kace & Andyslim feat. Sek - Unsigned',
+            'Carnivore - Dstruct - Unsigned',
+            'Implant Control - Pyro - Forthcoming Cabal',
+            'Particle Accelerator - Static & Rregula - Forthcoming Kinematic',
+            'Sunflower (Implex Remix) - Cyb Orc - Unsigned',
+            'The Flow - Noisia - Forthcoming Monkey Business',
+            'Goblyns - Espionage feat. Kace & Andyslim - Unsigned',
+            'Challenger - Phace - Forthcoming Leet',
+            'Octapod - Mayhem & ICBM - Forthcoming SLRCD',
+            'Untrue (Noisia Remix) - Soulproof - Forthcoming Rehab',
+            'Faithless - Quadrant - Unsigned',
+            'Mnemonix - Psidream - Forthcoming Nvision',
+            'Aztek - Cyb Orc - Unsigned',
+            'First Strike - Robot Death Squad - Unsigned',
+            'Depth Range - Kace - Unsigned',
+        ]
+    }]
+}];
---
+++ src/routes/archive/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+
+
+import { songArchive, setArchive } from '$lib/data/archive';
+
+let showTracklist = null;
+
+</script>
+
+<h2>Songs</h2>
+{#each songArchive as { year, songs }}
+    <div class="section">
+        <h3>{year}</h3>
+        <ul>
+            {#each songs as { href, title, artist, bitrate }}
+                <li><strong>{title}</strong> - {artist} | <a href={href}>mp3 ({bitrate})</a></li>
+            {/each}
+        </ul>
+    </div>
+{/each}
+
+<hr class="hr"/>
+
+<h2>Sets</h2>
+
+{#each setArchive as { year, sets }}
+    <div class="section">
+        <h3>{year}</h3>
+        <ul>
+            {#each sets as {href, title, artist, bitrate, tracklist }}
+                <li>
+                    <strong>{title}</strong> - {artist}
+                    |
+                    <a href={href}>mp3 ({bitrate})</a>
+                    |
+                    <button on:click={() => {
+                        showTracklist = showTracklist === title ? null : title;
+                    }}>
+                        tracklist
+                    </button>
+
+                    {#if showTracklist === title}
+                        <ol class="tracklist">
+                            {#each tracklist as track}
+                                <li>{track}</li>
+                            {/each}
+                        </ol>
+                    {/if}
+                </li>
+            {/each}
+        </ul>
+    </div>
+{/each}
+
+<style lang="postcss">
+    .section {
+        margin-bottom: 2em;
+
+        h3 {
+            margin-bottom: 0.3em;
+        }
+
+        ul {
+            border-left: solid 2px #999;
+            padding-left: 0.6em;
+            margin-left: 0.1em;
+        }
+    }
+
+    .tracklist {
+        font-size: 0.9em;;
+        margin-bottom: 2em;
+
+        li {
+            margin-left: 2em;
+        }
+    }
+
+    .hr {
+        margin: 3em 0;
+    }
+</style>
