dd8bae81a60624d27a811212eac576e920699b9b
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-10 17:04:29 -0800

    info page

---
+++ src/lib/components/links.svelte
@@ -0,0 +1,68 @@
+<script>
+    import soundcloudIcon from '$lib/icons/soundcloud.svg';
+    import bandcampIcon from '$lib/icons/bandcamp.svg';
+    import twitterIcon from '$lib/icons/twitter.svg';
+
+    const links = [{
+        href  : 'https://soundcloud.com/kace-1',
+        label : 'SoundCloud',
+        icon  : soundcloudIcon
+    }, {
+        href  : 'https://kace.bandcamp.com',
+        label : 'Bandcamp',
+        icon  : bandcampIcon
+    }, {
+        href  : 'https://twitter.com/kaceaudio',
+        label : 'Twitter',
+        icon  : twitterIcon
+    }, {
+        href  : 'https://kaceaudio.com/songs/Companion',
+        label : 'newest song: Companion'
+    }];
+</script>
+
+<ul class="links">
+    {#each links as { href, label, icon }}
+        <li>
+            <a href={href}>{label} <img src={icon} alt='' /></a>
+        </li>
+    {/each}
+</ul>
+
+<style lang="postcss">
+    .links {
+        max-width: 600px;
+        margin: 2em auto 0;
+
+        li a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            padding: 1em 0;
+            margin: 1.4em 0;
+
+            border: solid 1px #fff;
+            text-align: center;
+            text-decoration: none;
+
+            transition: background 0.15s, color 0.15s;
+
+            img {
+                margin-left: 0.6em;
+                height: 1.4em;
+
+                transition: filter 0.15s;
+            }
+
+            &:hover {
+                background: #eee;
+                color: #000;
+
+                img {
+                    filter: invert(1);
+                }
+            }
+        }
+    }
+</style>
---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    import Links from '$lib/components/links.svelte';
+</script>
+
+<div class="split">
+    <!-- <div>
+        <img src={'avatar.png'} alt={''} />
+    </div> -->
+    <div>
+        <dl>
+            <div>
+                <dt>name</dt>
+                <dd>Kevin A. Cameron</dd>
+            </div>
+
+            <div>
+                <dt>location</dt>
+                <dd>Seattle, WA</dd>
+            </div>
+
+            <div>
+                <dt>origin</dt>
+                <dd>Toronto, On</dd>
+            </div>
+
+            <div>
+                <dt>contact</dt>
+                <dd><a href='https://twitter.com/kaceaudio'>@kaceaudio</a></dd>
+            </div>
+        </dl>
+    </div>
+
+    <p>Composer, sound designer, and engineer. Started in audio as a drum &amp; bass dj and producer in Ontario Canada, outside Toronto. Received an Honours Diploma from The Harris Institute in producing/engineering. Had a short stint as the product specialist for Steinberg Canada, and recording engineer before moving to Seattle.</p>
+</div>
+
+<Links />
