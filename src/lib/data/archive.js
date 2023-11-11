5684043a22ec95619857655534bdc7bb851fe1e5
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-10 23:15:33 -0800

    more archive

--- src/lib/data/archive.js
+++ src/lib/data/archive.js
@@ -5,23 +5,27 @@ export const songArchive = [{
         artist  : 'Kace',
         bitrate : 320,
         href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Cell+Block+-+Kace.mp3'
-    }, {
+    },
+    {
         title   : "Breakin'Borders Dub",
         artist  : 'Kace & Andyslim',
         bitrate : 320,
         href    : "https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Breakin'Borders+Dub+-+Kace+%26+Andyslim.mp3"
-    }, {
+    },
+    {
         title   : 'Realize MC Flight',
         artist  : 'Kace & Andyslim',
         bitrate : 128,
         href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Realize+(feat+MC+Flight)+-+Kace+%26+Andyslim.mp3'
-    }, {
+    },
+    {
         title   : 'Freezeline',
         artist  : 'Kace & Andyslim',
         bitrate : 320,
         href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Freezeline+-+Kace+%26+Andyslim.mp3'
     }]
-}, {
+},
+{
     year  : 2004,
     songs : [{
         title   : 'Sub Scrypt',
@@ -29,7 +33,8 @@ export const songArchive = [{
         bitrate : 320,
         href    : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/songs/Sub+Scrypt+-+Kace+%26+Andyslim.mp3'
     }]
-}, {
+},
+{
     year  : 2006,
     songs : [{
         title   : 'Dust Bowl',
@@ -42,6 +47,14 @@ export const songArchive = [{
 export const setArchive = [{
     year : 2003,
     sets : [{
+        title    : 'Live @ Undertone',
+        artist   : 'Kace & Andyslim',
+        date     : 'February 28, 2003',
+        duration : '0:41:06',
+        bitrate  : 320,
+        href     : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Kace+%26+Andyslim+-+Undertone+-+February+28%2C+2003.mp3',
+    },
+    {
         title     : 'Oktober',
         artist    : 'Kace',
         bitrate   : 320,
@@ -64,12 +77,71 @@ export const setArchive = [{
             'Fatal Statistic - Raiden - TPT003 (46:28)',
             'Realize feat Flight - Kace & Andyslim (50:25)',
         ]
+    },
+    {
+        title     : 'Live @ Intelexual',
+        artist    : 'Kace & Andyslim',
+        date      : 'December 20, 2003',
+        duration  : '0:33:30',
+        bitrate   : 320,
+        href      : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Kace+%26+Andyslim+-+Live+%40+Intelexual+-+December+20%2C+2003.mp3',
+        tracklist : [
+            'Telemetrik - Nova [Telemetrik01]',
+            'Cause4Concern - Moongerm [Cause4Concern008]',
+            'Impulse & Submerged - Corrupt Souls [Subtitles032]',
+            'Axis of Evil - Drama [Cell001]',
+            'Kace & Andyslim feat. Flight MC - Realize [unreleased]',
+            'Bulletproof - Double Dutch [Cyanide005]',
+            'Kace & Andyslim - Freezeline [unreleases]',
+            'Illformants - The Bizarre [subtitles029]',
+            'Obsidian - Konflict [Deeper Realms 001]',
+            'Kace - Cell Block [unreleased]'
+        ]
     }]
-}, {
+},
+{
     year : 2004,
     sets : [{
+        title     : 'February Mix',
+        artist    : 'Kace & Andyslim',
+        date      : 'February 13, 2004',
+        duration  : '1:07:58',
+        bitrate   : 96,
+        href      : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Kace+%26+Andyslim+-+February+13%2C+2004.mp3',
+        tracklist : [
+            'Spectrata - Mansfield (Bes Remix) [Step2Zero]',
+            'Noisia - Silicon [Nerve009]',
+            'Divert - Hysteria [Unsigned]',
+            'Rob F & Impulse - Fallout [ShadowLaw001]',
+            'Psidream, Mayhem & Infiltrata - Avalanche [Payload003]',
+            'Pyro & Phono - Attack Sequence [Leet005]',
+            'Dissedent & Bes - Context [Unsigned]',
+            'Telemetrik - Monochrome [Telemetrik001]',
+            'Kace & Andyslim - Freezeline [Unsigned]',
+            'Optiv & Bulletproof - Camouflage [Cyanide007]',
+            'Kace - Cerebral [Unsigned]',
+            'Pyro - Sentenel [Nerve011]',
+            'Clandestine - Omicron [Unsigned]',
+            'Kiko & Submerged - Beyond Repair [Ohm015]',
+            'Eskmo - Sound [Unsigned]',
+            'N.Phect & Diz:play - Rotor [Protogen006]',
+            'Clandestine - Dreamstate [Unsigned]',
+            'Optiv, Concord Dawn & Bulletproof - Scorched Earth (Kemal remix) [Cyanide008]',
+        ]
+    },
+    {
+        title    : 'Live @ Van Goghs Ear',
+        artist   : 'Kace & Andyslim',
+        date     : 'May 29, 2004',
+        duration : '1:32:08',
+        bitrate  : 320,
+        href     : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Kace+%26+Andyslim+live+at+Van+Goghs+-+May+29+2004.mp3',
+        wav      : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Kace+%26+Andyslim+live+at+Van+Goghs+-+May+29+2004.wav'
+    },
+    {
         title     : 'Funk Fueled Fuck',
         artist    : 'Kace & Andyslim',
+        duration  : '1:11:30',
         bitrate   : 192,
         href      : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Funk+Fueled+Fuck+-+Kace+%26+Andyslim+(2004).mp3',
         tracklist : [
@@ -92,9 +164,11 @@ export const setArchive = [{
             'Dragline - Desimal - Unsigned (61:28)',
             'Haze - Kiko - ShadowLaw (65:34)'
         ]
-    }, {
+    },
+    {
         title     : 'Assimilation',
         artist    : 'Kace & Andyslim',
+        duration  : '1:19:42',
         bitrate   : 192,
         href      : 'https://kevinkace.s3-us-west-2.amazonaws.com/archive/sets/Assimilation+-+Kace+%26+Andyslim+(2004).mp3',
         tracklist : [
@@ -119,5 +193,38 @@ export const setArchive = [{
             'First Strike - Robot Death Squad - Unsigned',
             'Depth Range - Kace - Unsigned',
         ]
+    }, {
+        title     : 'Mix, seen?',
+        artist    : 'Kace & Andyslim',
+        date      : 'November 11, 2004',
+        duration  : '1:19:55',
+        bitrate   : 192,
+        href      : 'https://kevinkace.s3.us-west-2.amazonaws.com/archive/sets/Mix%2C+seen+-+Kace+%26+Andyslim+-+November+11%2C+2004.mp3',
+        tracklist : [
+            '[Genetix] - Red Square [Unsigned]',
+            'Noisia & Mayhem - Untitled [Unfinished]',
+            'Nokturnal - Pharnyx [Unsigned]',
+            'Vector Burn - Reflective [Technorganic007]',
+            'Krone - Phantom Magnet [Unsigned]',
+            'Mayhem & ICBM - Octopod [ForthcomingSLRCD001]',
+            'Noisia - Desolation [ForthcomingArmada]',
+            'Corrupt Souls - Trioxin 245 [ForthcomingMovingShadow]',
+            'Chris Su, Rob F & Mayhem - Untitled [Unfinished]',
+            'Kace & Andyslim - Sub Scrypt [Unsigned]',
+            'Dynafect - 2100 [Unsigned]',
+            'Moth - Sugar Crashing (KonflictRemix) [???]',
+            'Corrupt Souls - Machines [ForthcomingCircle]',
+            'Phace - Implant [ForthcomingSLRCD001]',
+            'Noisia - Hubcap [ForthcomingSubtitles]',
+            'Psidream - Mnemonix [ForthcomingNvision]',
+            'Noisia - The Flow [ForthcomingMonkeyBusiness]',
+            'Kace - Cell Block [Unsigned]',
+            'Sinthetix - Chain Reaction [NeverReleased]',
+            'Corrupt Souls - Drop Zone [ForthcomingMovingShadow]',
+            'Mayhem, Psidream & Evol Intent - Assimilation [EvolIntent]',
+            'Kace - Cerebral [Unsigned]',
+            'Corrupt Souls - Broadcast [ForthcomingMovingShadow]'
+        ]
+
     }]
 }];
